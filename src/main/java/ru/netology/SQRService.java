350a889d520c69d66fd74e006b265d33ef0b0a39
bubekov1990 <bobbob1990@mail.ru>
2022-06-15 14:05:55 +0300

    Сode changes

--- src/main/java/ru/netology/SQRService.java
+++ src/main/java/ru/netology/SQRService.java
@@ -2,13 +2,14 @@ package ru.netology;
 
 public class SQRService {
 
-    public int howManySquares(int minimalimit, int maxsimumlimit) {
+    public int howManySquares(int lower, int upper) {
         int counter = 0;
-        for (int i = 10; i < 100; i++) {
-            if (i * i > minimalimit && i * i < maxsimumlimit) {
-                counter++;
+        for (int i = 10; i <= 99; i++) {
+            if ((i * i <= upper && i * i >= lower)) {
+                counter = counter + 1;
             }
         }
+        System.out.println(counter);
         return counter;
     }
 }
--- src/test/java/ru/netology/SQRServiceTest.java
+++ src/test/java/ru/netology/SQRServiceTest.java
@@ -1,57 +1,28 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
-    @Test
-    void sqrtCeil() {
-        SQRService service = new SQRService();
-        int expected = 3;
-        int actual = service.howManySquares(200, 300);
-        assertEquals(expected, actual);
-    }
 
-    @Test
-    void sqrtCeilAnotherRange() {
-        SQRService service = new SQRService();
-        int expected = 11;
-        int actual = service.howManySquares(100, 400);
-        assertEquals(expected, actual);
-    }
+    @ParameterizedTest
+    @CsvSource(value = {
+            "from 10 to 99 range 0-10, 0, 0,10",
+            "from 10 to 99 range 0-100, 1, 0,100",
+            "from 10 to 99 range 400-800, 9, 400,800",
+            "from 10 to 99 range 1000-5000, 39, 1000,5000",
+            "from 10 to 99 range 0-9000, 85, 0,9000",
+            "from 10 to 99 range -10-300, 0, -10,-300"
+    })
 
-    @Test
-    void sqrtCeilMaxNumber() {
+    public void calculateSqrt(String testName, int expected, int lower, int upper) {
         SQRService service = new SQRService();
-        int expected = 1;
-        int actual = service.howManySquares(9700, 9900);
-        assertEquals(expected, actual);
-    }
 
-    @Test
-    void sqrtCeilUnderMaxNumber() {
-        SQRService service = new SQRService();
-        int expected = 0;
-        int actual = service.howManySquares(9900, 10000);
-        assertEquals(expected, actual);
-    }
+        int actual = service.howManySquares(lower, upper);
 
-    @Test
-    void sqrtCeilMinNumber() {
-        SQRService service = new SQRService();
-        int expected = 1;
-        int actual = service.howManySquares(80, 110);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
-
-    @Test
-    void sqrtCeilUnderMinNumber() {
-        SQRService service = new SQRService();
-        int expected = 0;
-        int actual = service.howManySquares(40, 99);
-        assertEquals(expected, actual);
-    }
-
 
 }
